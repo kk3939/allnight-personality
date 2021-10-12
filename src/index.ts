fd8c105ffd82d3238c9e64d95555b2121a230e2d
kk3939 <kyo51310@gmail.com>
2021-10-12 20:25:37 +0900

    パーソナリティ一覧を取得機能実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("こんにちは");
+import puppeteer from "puppeteer";
+import type { Browser, Page } from "puppeteer";
+// import inquirer from "inquirer";
+
+const url: string = "https://www.allnightnippon.com/personality.html";
+
+const main = async (): Promise<void> => {
+  const browser: Browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  // const elem = await page.$$("dd");
+  // console.log(elem);
+
+  const scrapingData = await page.evaluate(() => {
+    const array: any = [];
+    const nodeList = document.querySelectorAll("dd");
+    nodeList.forEach((elem) => {
+      array.push(elem.textContent)
+    });
+    return array;
+  });
+  console.log(scrapingData);
+
+};
+
+main();
