d053d40e440a20bedf463d0dff570912e345554b
kk3939 <kyo51310@gmail.com>
2021-10-15 21:20:17 +0900

    constractorの部分の非同期を今後実装

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
 import puppeteer from "puppeteer";
 import type { Browser, Page } from "puppeteer";
-// import inquirer from "inquirer";
 
-const url: string = "https://www.allnightnippon.com/personality.html";
+export class AllnightPersonality {
+  constructor() { }
+  async fetchPersonality() {
+    return new Promise(async (resolve, reject) => {
+      const url: string = "https://www.allnightnippon.com/personality.html";
+      const browser: Browser = await puppeteer.launch();
+      const page: Page = await browser.newPage();
+      try {
+        await page.goto(url)
+        const scrapingData = await page.evaluate(() => {
+          const array: string[] = [];
+          const nodeList: NodeListOf<HTMLElement> = document.querySelectorAll("dd");
+          nodeList.forEach((elem: HTMLElement, index) => {
+            if (!elem.textContent) {
+              array.push("can not get personality name.");
+            } else {
+              array.push(elem.textContent);
+            }
+          });
+          resolve(array);
+        });
+        return scrapingData;
 
-const main = async (): Promise<void> => {
-  const browser: Browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  // const elem = await page.$$("dd");
-  // console.log(elem);
+      } catch (e) {
+      }
+    })
+  };
+}
 
-  const scrapingData = await page.evaluate(() => {
-    const array: any = [];
-    const nodeList = document.querySelectorAll("dd");
-    nodeList.forEach((elem) => {
-      array.push(elem.textContent)
-    });
-    return array;
-  });
-  console.log(scrapingData);
+const main = new AllnightPersonality;
+console.log(main.fetchPersonality());
 
-};
 
-main();
+// const fetchPersonality = async () => {
+//   const url: string = "https://www.allnightnippon.com/personality.html";
+//   const browser: Browser = await puppeteer.launch();
+//   const page: Page = await browser.newPage();
+//   try {
+//     await page.goto(url);
+//     const scrapingData = await page.evaluate(() => {
+//       const array: string[] = [];
+//       const nodeList: NodeListOf<HTMLElement> = document.querySelectorAll("dd");
+//       nodeList.forEach((elem: HTMLElement, index) => {
+//         if (!elem.textContent) {
+//           array.push("can not get personality name.");
+//         } else {
+//           array.push(elem.textContent);
+//         }
+//       });
+//       return array;
+//     });
+//     return scrapingData;
+//   } catch (e) {
+//     return e;
+//   }
+// };
+
+// fetchPersonality();
